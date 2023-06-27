d431083d05002a5bc805242613a32be6e29b5417
Ayham Alhomsy <ay.alhomsy@gmail.com>
2023-06-27 09:04:22 +0300

    fix

--- MainApp.tsx
+++ MainApp.tsx
@@ -5,14 +5,16 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 import App from './src/App';
 import ToolBar from './src/components/Layout/ToolBar';
 import BottomBar from './src/components/Layout/BottomBar';
+import {Platform} from 'react-native';
 
 const MainApp = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    paddingTop: Platform.OS === 'ios' ? 40 : 0,
   };
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={[backgroundStyle]}>
       <ToolBar />
       <App />
       <BottomBar />
